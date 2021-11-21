da04252609b1bab8a41e1717cc5eee3dd0198e8c
anusha@peddoju <83536141+anusha-peddoju@users.noreply.github.com>
2021-11-21 22:15:21 +0530

    Update change_password.sh

--- change_password.sh
+++ change_password.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-read -s -p "enter terminal password: " password
+
+read -s -p "Enter Terminal Password: " password #Reading Password from User
 echo
-sed "s/user/$password/g" lock.sh > setlock
-rm lock.sh
-sudo chmod +x  setlock
-sudo mv setlock /usr/bin
-sudo echo setlock  >> $HOME/Student .bashrc
-sudo rm -r ../terminal-lock 
+sed "s/user/$password/g" lock.sh > setlock #Replacing current password in lock.sh file and putting it in setlock file.
+rm lock.sh #removing lock.sh file
+sudo chmod +x  setlock #giving excutable permission to setlock
+sudo mv setlock /usr/bin #moving setlock to /usr/bin Directory
+sudo echo setlock  >> $HOME/.bashrc #adding setlock file to .bashrc file in home to run start in terminal.
+#here bashrc file only runs the file present in /usr/bin.
+sudo rm -r ../set_terminal_lock #removing set_terminal lock directory.
